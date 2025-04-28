39a5d108042e9d89880d659f960fdbd99c0cb55f
jodeon <deon_jsn@hotmail.com>
2025-04-28 17:04:48 -0300

    add rotas

--- app.py
+++ app.py
@@ -4,5 +4,20 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
+@app.route('/payments/pix', methods=['POST'])
+def create_payments_pix():
+    return jsonify({"message": "The payment has been created"})
+
+
+@app.route('/payments/pix/confirmation', methods=['POST'])
+def pix_confirmation():
+    return jsonify({"message": "The payments has been confirmed"})
+
+
+@app.route('/payments/pix/<int:payments_id>', methods=['GET'])
+def payments_pix_page(payments_id):
+    return 'pagamento pix'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
