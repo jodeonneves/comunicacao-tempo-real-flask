52ce779ed22aaba9f61aecf68983e1b7f001bfd6
jodeon <deon_jsn@hotmail.com>
2025-05-14 17:27:17 -0300

    create payment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
 from db_models.payments import Payments
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
@@ -13,7 +14,21 @@ db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payments_pix():
-    return jsonify({"message": "The payment has been created"})
+    data = request.get_json()
+
+    # validações
+    if 'value' not in data:
+        return jsonify({"message": "Invalid value"}), 400
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payments(value=data['value'], expiration_date=expiration_date)
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({"message": "The payment has been created",
+                   "payment": new_payment.to_dict()})
 
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
